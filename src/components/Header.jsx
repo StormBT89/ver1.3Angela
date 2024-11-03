987e3145b5577517b141adfdb8fa81573c1a1ce8
StormBT89 <glavevski.pece@gmail.com>
2024-11-03 12:44:03 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Форма за внесување на податоци - тест верзија 1.3 Ангела
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
